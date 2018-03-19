2eea2a2bb462877f4b6341df7bd43ef803adde8e
casey <cwt59@humboldt.edu>
2018-03-19 10:48:31 -0700

    added Service.class for background. Passing Intents from MainActivity to Service. ToDO: set parameters and link to the intents.

--- projects/Project 4/FastBreak/app/src/main/java/com/example/cwt59/fastbreak/StartFast.java
+++ projects/Project 4/FastBreak/app/src/main/java/com/example/cwt59/fastbreak/StartFast.java
@@ -1,7 +1,10 @@
 package com.example.cwt59.fastbreak;
 
+import android.app.Service;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class StartFast extends AppCompatActivity {
 
@@ -11,4 +14,18 @@ public class StartFast extends AppCompatActivity {
         setContentView(R.layout.activity_start_fast);
 
     }
+
+    public void Begin(View view){
+        //Intent intent = new Intent( this, StartFast.class);
+        // below is how to pass an intent for use in a Service to run in the backgroun
+        Intent intent =new Intent(this, Service.class);
+        intent.putExtra("data", data);
+
+
+        this.startService(intent);
+
+        startActivity(intent);
+    }
+
+
 }
