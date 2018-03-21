5eb04b93928ce53a7be7a3e490cfdb2f1cd067c0
casey <cwt59@humboldt.edu>
2018-03-21 14:48:14 -0700

    Starting from scratch with the Service. step by step

--- projects/Project 4/FastBreak/app/src/main/java/com/example/cwt59/fastbreak/StartFast.java
+++ projects/Project 4/FastBreak/app/src/main/java/com/example/cwt59/fastbreak/StartFast.java
@@ -11,6 +11,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.util.Log;
+import android.widget.Button;
 import android.widget.TextView;
 
 
@@ -20,33 +21,10 @@ public class StartFast extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_fast);
-        startService(new Intent(this, MyService.class));
-        Log.i("Started service", "hello started service...");
-        registerReceiver(br, new IntentFilter("COUNTDOWN_UPDATED"));
-    }
 
-    private BroadcastReceiver br = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            intent.getExtras();
-            long millisUntilFinished = intent.getLongExtra("countdown",0);
-            String time = Long.toString((millisUntilFinished));
-            TextView tv = findViewById(R.id.timeView1);
-            tv.setText(time);
+
         }
-    };
-
-    public void BeginFast(View view){
-        //Intent intent = new Intent( this, StartFast.class);
-        // below is how to pass an intent for use in a Service to run in the backgroun
-       Intent intent =new Intent(this, MyService.class);
-       startService(intent);
-         //   intent.putExtra() // putExtra longs ...will do after static run succeeds
-        //intent.putExtra("data", data); //adding the data
-
-        Intent intent1 = new Intent(this, Heart.class);
-        startActivity(intent1);
-    }
 
 
 }
+
